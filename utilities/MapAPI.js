280f8884f72dcf586a71aca16fd5bd424b742647
Krystian Zakryś <krystian.zakrys@gmail.com>
2018-06-11 11:30:46 +0200

    Fixed dynamic locals loading

--- utilities/MapAPI.js
+++ utilities/MapAPI.js
@@ -1,14 +1,11 @@
 var mapAPI = {
     getLocals(lat, long, radius){
-        alert(lat);
-        alert(long);
         var temp = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=LOCALIZATION&radius=RADIUS&type=restaurant&key=AIzaSyAjzqwm143qvqKBEDizrJTeeSDauJSmpvA';
-        var local = lat+'%'+long;//'53.118289%2C23.149720';
-        var rad = radius;
+        var local = lat+','+long;//'53.118289%2C23.149720';
+        var rad = radius*100;
 
         var url = temp.replace("LOCALIZATION",local);
         url = url.replace("RADIUS",rad);
-        alert(url);
         return fetch(url).then((res)=> res.json());
     }
 };
